c09bd93d31c9cc2ed2dfd6c3dba8874e566840b8
toyohappy <sarahrusso678@gmail.com>
2026-01-20 15:10:53 +0900

    Delete test directory

--- test/js/02dgdsg3d.js
+++
@@ -1,5 +0,0 @@
-
-      window.onload = function() {
-        window.moveTo(0, 0);
-        window.resizeTo(screen.availWidth, screen.availHeight)
-      };
--- test/js/10dgsjsd.js
+++
@@ -1,5 +0,0 @@
-
-  		const searchParams = new URLSearchParams(window.location.search);
-  		let dda = '(0101) 201 212 4547';
-  		let bcda = dda;
-  	 
